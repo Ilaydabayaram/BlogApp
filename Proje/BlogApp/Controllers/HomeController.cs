8ea908f28eb6785e890b1ae6e4b7e8c712fc614a
Ilaydabayaram <ilaydabay3404@outlook.com>
2025-11-30 16:00:49 +0300

    Gereksiz dosyalar silindi

---
+++ Proje/BlogApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using BlogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
